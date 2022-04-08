5e965a06f7121e88bbd0a41aa435c64049c32600
a-chacon <andres.ch@protonmail.com>
2022-04-08 18:41:48 -0400

    feat: controller with the show method for a short url

--- app/controllers/rails_url_shortener/urls_controller.rb
+++ app/controllers/rails_url_shortener/urls_controller.rb
@@ -6,13 +6,10 @@ module RailsUrlShortener
     include Rails.application.routes.url_helpers
 
     def show
-      # token = ::Shortener::ShortenedUrl.extract_token(params[:id])
-      # track = Shortener.ignore_robots.blank? || request.human?
-      # url   = ::Shortener::ShortenedUrl.fetch_with_token(token: token, additional_params: params, track: track)
       # Find
       url = Url.find_by_key(params[:key])
       # Save visit
-      Visit.parse_and_save(request)
+      Visit.create(url: url, meta: request)
       redirect_to url.url, status: :moved_permanently
     end
   end
--- test/controllers/rails_url_shortener/urls_controller_test.rb
+++ test/controllers/rails_url_shortener/urls_controller_test.rb
@@ -1,14 +1,27 @@
-require "test_helper"
+require 'test_helper'
 
 module RailsUrlShortener
   class UrlsControllerTest < ActionDispatch::IntegrationTest
     include Engine.routes.url_helpers
 
-    test "show" do
-      get "/rails_url_shortener/as12as", headers:{
-        user_agent: "Mozilla/5.0 (X11; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0"
-      }
-      assert_response :moved_permanently
+    test 'show' do
+      assert_difference 'Visit.count', 1 do
+        get "/rails_url_shortener/#{rails_url_shortener_urls(:one).key}", headers: {
+          user_agent: 'Mozilla/5.0 (X11; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0'
+        }
+        assert_response :moved_permanently
+        assert_redirected_to rails_url_shortener_urls(:one).url
+      end
+    end
+
+    test 'show whit a not existing key' do
+      assert_difference 'Visit.count', 1 do
+        get "/rails_url_shortener/noexist", headers: {
+          user_agent: 'Mozilla/5.0 (X11; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0'
+        }
+        assert_response :moved_permanently
+        assert_redirected_to "/"
+      end
     end
   end
 end
