9cf6f11bfd9a0a649c2b9412662172097fb9ce25
a-chacon <andres.ch@protonmail.com>
2022-04-12 16:34:23 -0400

    refatot: use root path

--- test/controllers/rails_url_shortener/urls_controller_test.rb
+++ test/controllers/rails_url_shortener/urls_controller_test.rb
@@ -6,7 +6,7 @@ module RailsUrlShortener
 
     test 'show' do
       assert_difference 'Visit.count', 1 do
-        get "/rails_url_shortener/#{rails_url_shortener_urls(:one).key}", headers: {
+        get "/#{rails_url_shortener_urls(:one).key}", headers: {
           user_agent: 'Mozilla/5.0 (X11; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0'
         }
         assert_response :moved_permanently
@@ -16,7 +16,7 @@ module RailsUrlShortener
 
     test 'show whit a not existing key' do
       assert_no_difference 'Visit.count', 1 do
-        get "/rails_url_shortener/noexist", headers: {
+        get "/noexist", headers: {
           user_agent: 'Mozilla/5.0 (X11; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0'
         }
         assert_response :moved_permanently
