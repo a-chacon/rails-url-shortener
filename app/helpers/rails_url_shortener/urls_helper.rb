1b76805ec194d908c739b5adb7ac9942719dd551
a-chacon <andres.ch@protonmail.com>
2022-04-12 16:34:33 -0400

    feat: helper method

--- app/helpers/rails_url_shortener/urls_helper.rb
+++ app/helpers/rails_url_shortener/urls_helper.rb
@@ -1,4 +1,28 @@
 module RailsUrlShortener
   module UrlsHelper
+    def short_url(url, owner: nil, key: nil, expires_at: nil, category: nil, url_options: {})
+      url_object = Url.generate(
+        url,
+        owner: owner,
+        key: key,
+        expires_at: expires_at,
+        category: category
+      )
+
+      if url_object.errors.empty?
+        # This must be fixed
+        # the url_for helper must generate the url
+        # options = {
+        #   controller: "rails_url_shortener_url/urls",
+        #   action: "show",
+        #   key: url.key
+        # }.merge(url_options)
+
+        # url_for(options)
+        rails_url_shortener_url + url_object.key
+      else
+        url
+      end
+    end
   end
 end
---
+++ test/helpers/rails_url_shortener/urls_helper_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+module RailsUrlShortener
+  class UrlsHelperTest < ActionView::TestCase
+    test 'generate url' do
+      url = 'https://www.github.com/a-chacon/rails_url_shortener/asdss'
+      assert_equal short_url(url), "http://test.host/#{Url.where(url: url).first.key}"
+    end
+
+    test 'generate with existing key' do
+      url = 'https://www.github.com/a-chacon/rails_url_shortener/asdss'
+      assert_equal short_url(url, key: "aE1122"), url
+    end
+
+    test 'generate url with owner' do
+      url = 'https://www.github.com/a-chacon/rails_url_shortener/asdss'
+      assert_equal short_url(url, owner: users(:one)), "http://test.host/#{Url.where(url: url).first.key}"
+      assert_equal Url.where(url: url).first.owner, users(:one)
+    end
+
+    test 'generate url with custom key' do
+      url = 'https://www.github.com/a-chacon/rails_url_shortener/asdss'
+      assert_equal short_url(url, key: "asd"), "http://test.host/#{Url.where(url: url).first.key}"
+    end
+
+    test 'generate url with expires at' do
+      url = 'https://www.github.com/a-chacon/rails_url_shortener/asdss'
+      assert_equal short_url(url, expires_at: Time.now + 2.hours), "http://test.host/#{Url.where(url: url).first.key}"
+      assert_equal Url.where(url: url).first.expires_at.utc.ceil, (Time.now.utc + 2.hours).ceil
+    end
+
+    test 'generate url with category' do
+      url = 'https://www.github.com/a-chacon/rails_url_shortener/asdss'
+      assert_equal short_url(url, category: 'free'), "http://test.host/#{Url.where(url: url).first.key}"
+      assert_equal Url.where(url: url).first.category, 'free'
+    end
+  end
+end
