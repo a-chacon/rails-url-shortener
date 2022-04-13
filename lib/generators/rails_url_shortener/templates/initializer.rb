fa8a97dbbcdc655820cb9e75246ce2a682395902
a-chacon <andres.ch@protonmail.com>
2022-04-13 11:44:04 -0400

    fix: add comments to initializer template

--- lib/generators/rails_url_shortener/templates/initializer.rb
+++ lib/generators/rails_url_shortener/templates/initializer.rb
@@ -4,8 +4,8 @@ CHARSETS = {
   alphanumcase: ('A'..'Z').to_a + ('a'..'z').to_a  + (0..9).to_a
 }
 
-RailsUrlShortener.default_redirect = "/"
-RailsUrlShortener.charset = CHARSETS[:alphanumcase]
-RailsUrlShortener.key_length = 6
-RailsUrlShortener.minimum_key_length = 3
-RailsUrlShortener.save_bots_visits = false
+RailsUrlShortener.default_redirect = "/"            # where the users are redirect if the link doesn't exists or is expired.
+RailsUrlShortener.charset = CHARSETS[:alphanumcase] # used for generate the keys, better long.
+RailsUrlShortener.key_length = 6                    # Key length for random generator
+RailsUrlShortener.minimum_key_length = 3            # minimun permited for a key
+RailsUrlShortener.save_bots_visits = false          # if save bots visits
