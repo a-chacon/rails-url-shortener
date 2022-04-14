a581d1e9ee8477dd0b0243ac0e40eea277ef89d2
a-chacon <andres.ch@protonmail.com>
2022-04-14 10:56:08 -0400

    docs: update template for configurations with host

--- lib/generators/rails_url_shortener/templates/initializer.rb
+++ lib/generators/rails_url_shortener/templates/initializer.rb
@@ -4,6 +4,7 @@ CHARSETS = {
   alphanumcase: ('A'..'Z').to_a + ('a'..'z').to_a  + (0..9).to_a
 }
 
+RailsUrlShortener.host = "localhost:3000"           # the host used for the helper
 RailsUrlShortener.default_redirect = "/"            # where the users are redirect if the link doesn't exists or is expired.
 RailsUrlShortener.charset = CHARSETS[:alphanumcase] # used for generate the keys, better long.
 RailsUrlShortener.key_length = 6                    # Key length for random generator
