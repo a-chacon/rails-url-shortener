eeebbbd1ed3774e4d51029eb35cd9406d4191f5b
a-chacon <andres.ch@protonmail.com>
2022-05-30 15:20:47 -0400

    fix: ip crawler job complexity

--- app/models/rails_url_shortener/ipgeo.rb
+++ app/models/rails_url_shortener/ipgeo.rb
@@ -3,5 +3,12 @@
 module RailsUrlShortener
   class Ipgeo < ApplicationRecord
     has_many :visits, dependent: :nullify
+
+    def update_from_remote
+      @ip = HTTP.get("http://ip-api.com/json/#{ip}?fields=status,message,country,countryCode,region,regionName,city,zip,lat,lon,timezone,isp,org,as,mobile,proxy,hosting,query")
+      return unless @ip.code == 200
+
+      update(JSON.parse(@ip.body).transform_keys { |key| key.to_s.underscore }.slice(*Ipgeo.column_names))
+    end
   end
 end
